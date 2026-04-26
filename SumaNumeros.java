917ba9571e3f09e7484ccf56a12c42db54d1f1e4
jpg-ms-yt <tinovalegomez@gmail.com>
2026-04-25 19:22:08 -0500

    Add files via upload

---
+++ SumaNumeros.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        int a=15;
+        int b=27;
+        int suma=a+b;
+        System.out.println("La suma de " + a + " +" + b + " = " + suma);
+    }
+}
+       
