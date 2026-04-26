8713b66c99492d95c77ec35883e5d977673aca14
jpg-ms-yt <tinovalegomez@gmail.com>
2026-04-25 21:35:18 -0500

    Add files via upload

---
+++ CuentaRegresiva.java
@@ -0,0 +1,9 @@
+public class CuentaRegresiva {
+    public static void main(String[] args) {
+        System.out.println("Cuenta regresiva:");
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+            System.out.println("¡Despegue!");
+    }
+}
