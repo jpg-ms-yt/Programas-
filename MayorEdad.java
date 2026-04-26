020452208b59b501c985ceab54476013e0f9bd02
jpg-ms-yt <tinovalegomez@gmail.com>
2026-04-25 19:42:02 -0500

    Add files via upload

---
+++ MayorEdad.java
@@ -0,0 +1,11 @@
+public class MayorEdad {
+    public static void main(String[] args) {
+        int edad = 18; // Puedes cambiar este valor para probar con diferentes edades
+
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad.");
+        } else {
+            System.out.println("Eres menor de edad.");
+        }
+    }
+}
